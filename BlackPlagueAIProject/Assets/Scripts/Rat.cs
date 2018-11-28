f3604ebc14450f913e21a9c1887def08455e3561
ekarlson238 <ekarlson238@gmail.com>
2018-11-27 18:05:23 -0600

    testing movement code
    (its not working)

--- BlackPlagueAIProject/Assets/Scripts/Rat.cs
+++ BlackPlagueAIProject/Assets/Scripts/Rat.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class Rat : MonoBehaviour {
 
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private float minRandomFloat;
+    [SerializeField]
+    private float maxRandomFloat;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate ()
+    {
+        rb.velocity = RandomVector(minRandomFloat, maxRandomFloat);
 	}
+
+    private Vector2 RandomVector(float min, float max)
+    {
+        var x = Random.Range(min, max);
+        var y = Random.Range(min, max);
+        return new Vector2();
+    }
 }
