fbaa7e313f73222733b7a48916069f906e13a528
IanJeannin <36770934+IanJeannin@users.noreply.github.com>
2018-11-30 15:36:46 -0600

    Add changes to movement based on other entities in proximity, start testing movement

--- BlackPlagueAIProject/Assets/Scripts/Human.cs
+++ BlackPlagueAIProject/Assets/Scripts/Human.cs
@@ -20,10 +20,72 @@ public class Human : MonoBehaviour
     float directionUpdateTime; //How long before the direction is updated
     private float countDownTime; //Used to copy directionUpdateTime for decrementing
     private Rigidbody2D rb;
-    private  float xMin = -1;
-    private float xMax = 1;
-    private float yMin = -1;
-    private float yMax = 1;
+    [SerializeField]
+    float xMin = -1;
+    [SerializeField]
+    float xMax = 1;
+    [SerializeField]
+    float yMin = -1;
+    [SerializeField]
+    float yMax = 1;
+    
+    private float ratVariable = 0.75f; //Used to make human less likely to move towards rats in detection radius
+    private float humanVariable = 0.5f; //Used to make human more likely to move towards other humans in detection radius
+    private float infectedVariable = 0.8f; //Used to make human less likely to move towards infected in detection radius
+    [SerializeField]
+    ContactFilter2D uninfectedContactFilter;
+    [SerializeField]
+    ContactFilter2D infectedContactFilter;
+    [SerializeField]
+    ContactFilter2D ratContactFilter;
+
+    private bool upRatDetected;
+    private bool downRatDetected;
+    private bool leftRatDetected;
+    private bool rightRatDetected;
+
+    private bool upHumanDetected;
+    private bool downHumanDetected;
+    private bool leftHumanDetected;
+    private bool rightHumanDetected;
+
+    private bool upInfectedDetected;
+    private bool downInfectedDetected;
+    private bool leftInfectedDetected;
+    private bool rightInfectedDetected;
+
+    private Collider2D[] upRatDetection = new Collider2D[50];
+    private Collider2D[] downRatDetection = new Collider2D[50];
+    private Collider2D[] leftRatDetection = new Collider2D[50];
+    private Collider2D[] rightRatDetection = new Collider2D[50];
+
+    private Collider2D[] upHumanDetection = new Collider2D[50];
+    private Collider2D[] downHumanDetection = new Collider2D[50];
+    private Collider2D[] leftHumanDetection = new Collider2D[50];
+    private Collider2D[] rightHumanDetection = new Collider2D[50];
+
+    private Collider2D[] upInfectedDetection = new Collider2D[50];
+    private Collider2D[] downInfectedDetection = new Collider2D[50];
+    private Collider2D[] leftInfectedDetection = new Collider2D[50];
+    private Collider2D[] rightInfectedDetection = new Collider2D[50];
+
+    //DetectionTriggers
+    [SerializeField]
+    Collider2D upRat;
+    [SerializeField]
+    Collider2D downRat;
+    [SerializeField]
+    Collider2D leftRat;
+    [SerializeField]
+    Collider2D rightRat;
+    [SerializeField]
+    Collider2D upHuman;
+    [SerializeField]
+    Collider2D downHuman;
+    [SerializeField]
+    Collider2D leftHuman;
+    [SerializeField]
+    Collider2D rightHuman;
 
 
     private void Start()
@@ -44,7 +106,72 @@ public class Human : MonoBehaviour
 
     public void Move()
     {
-        rb.velocity=RandomVector(xMin, xMax, yMin, yMax);
+        ColliderCheck();
+        Debug.Log(upHumanDetected);
+        Debug.Log(leftHumanDetected);
+        Debug.Log(rightHumanDetected);
+        Debug.Log(downHumanDetected);
+        //Used to temporarily change x/y max/min
+        float tempXMin = xMin;
+         float tempXMax = xMax;
+         float tempYMin = yMin;
+         float tempYMax = yMax;
+        //===========================================================================================================
+        //While statements used to decrease chance of moving towards rat, and increase chance of moving towards human
+        //===========================================================================================================
+        
+        if (upHumanDetected==true)
+        {
+            tempYMin = -0.5f;
+        }
+        if (downHumanDetected == true)
+        {
+            tempYMax = 0.5f;
+        }
+        if (leftHumanDetected == true)
+        {
+            tempXMax = 0.5f;
+        }
+        if (rightHumanDetected == true)
+        {
+            tempXMin = -0.5f;
+        }
+
+        if (upRatDetected == true)
+        {
+            tempYMax = 0.4f;
+        }
+        if (downRatDetected == true)
+        {
+            tempYMin = -0.4f;
+        }
+        if (leftRatDetected == true)
+        {
+            tempXMin = -0.4f;
+        }
+        if (rightRatDetected == true)
+        {
+            tempXMax = 0.4f;
+        }
+
+
+        if (upInfectedDetected == true)
+        {
+            tempYMax = 0.2f;
+        }
+        if (downInfectedDetected == true)
+        {
+            tempYMin = -0.2f;
+        }
+        if (leftInfectedDetected == true)
+        {
+            tempXMin = -0.2f;
+        }
+        if (rightInfectedDetected == true)
+        {
+            tempXMax = 0.2f;
+        }
+        rb.velocity=RandomVector(tempXMin, tempXMax, tempYMin, tempYMax);
 
     }
 
@@ -53,6 +180,37 @@ public class Human : MonoBehaviour
         var x = Random.Range(xMin, xMax);
         var y = Random.Range(yMin, yMax);
         return new Vector2(x*speed, y*speed);
+    }
+
+    private void ColliderCheck()
+    {
+        int humans = 0;
+        int infected = 0;
+        if (gameObject.layer == LayerMask.NameToLayer("Uninfected"))
+        {
+            humans = 1;
+            infected = 0;
+        }
+        if(gameObject.layer==LayerMask.NameToLayer("Infected"))
+        {
+            infected = 1;
+            humans = 0;
+        }
+        upRatDetected = upRat.OverlapCollider(ratContactFilter, upRatDetection) > 0;
+        downRatDetected = downRat.OverlapCollider(ratContactFilter, downRatDetection) > 0;
+        leftRatDetected = leftRat.OverlapCollider(ratContactFilter, leftRatDetection) > 0;
+        rightRatDetected = rightRat.OverlapCollider(ratContactFilter, rightRatDetection) > 0;
+
+        upHumanDetected = upHuman.OverlapCollider(uninfectedContactFilter, upHumanDetection) > humans;
+        downHumanDetected = downHuman.OverlapCollider(uninfectedContactFilter, downHumanDetection) > humans;
+        leftHumanDetected = leftHuman.OverlapCollider(uninfectedContactFilter, leftHumanDetection) > humans;
+        rightHumanDetected = rightHuman.OverlapCollider(uninfectedContactFilter, rightHumanDetection) > humans;
+
+        //Uses the same trigger as uninfected
+        upInfectedDetected = upHuman.OverlapCollider(infectedContactFilter, upInfectedDetection) > infected;
+        downInfectedDetected = downHuman.OverlapCollider(infectedContactFilter, downInfectedDetection) > infected;
+        leftInfectedDetected = leftHuman.OverlapCollider(infectedContactFilter, leftInfectedDetection) > infected;
+        rightInfectedDetected = rightHuman.OverlapCollider(infectedContactFilter, rightInfectedDetection) > infected;
     }
 
 }
