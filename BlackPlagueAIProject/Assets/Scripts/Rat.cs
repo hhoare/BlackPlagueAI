45080f4675c02b073eba30dd9bf618508007cfa6
ekarlson238 <43217591+ekarlson238@users.noreply.github.com>
2018-11-30 11:24:28 -0600

    rat movement is working

--- BlackPlagueAIProject/Assets/Scripts/Rat.cs
+++ BlackPlagueAIProject/Assets/Scripts/Rat.cs
@@ -5,28 +5,44 @@ using UnityEngine;
 public class Rat : MonoBehaviour {
 
     private Rigidbody2D rb;
+    private float countdownTime;
 
+    [SerializeField]
+    private float time;
     [SerializeField]
     private float minRandomFloat;
     [SerializeField]
     private float maxRandomFloat;
+    [SerializeField]
+    private float speed;
 
 	// Use this for initialization
 	void Start ()
     {
         rb = GetComponent<Rigidbody2D>();
+        countdownTime = time;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate ()
     {
+        countdownTime -= Time.deltaTime;
+        if (countdownTime <= 0)
+        {
+            Move();
+            countdownTime = time;
+        }
+    }
+
+    private void Move()
+    {
         rb.velocity = RandomVector(minRandomFloat, maxRandomFloat);
-	}
+    }
 
     private Vector2 RandomVector(float min, float max)
     {
         var x = Random.Range(min, max);
         var y = Random.Range(min, max);
-        return new Vector2();
+        return new Vector2(x*speed, y*speed);
     }
 }
